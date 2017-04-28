d47b067abe1b192523d33256afeac13abb67b582
hcvo <jba152@att.net>
2017-04-27 21:30:44 -0500

    Add the controller and the httpGet

---
+++ src/MyCodeCamp/Controllers/CampsController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyCodeCamp.Controllers
+{
+    public class CampsController : Controller
+    {
+
+        public IActionResult Get()
+        {
+            return Ok(new { Name = "HV", FavoriteColor = "Blue" });
+        }
+        
+    }
+}
