c8ffc54f442109a1f0b6b668d54e9e6037bee6cf
h1416 <hieuvo1416@gmail.com>
2017-05-03 14:53:52 -0500

    Added the post method for speaker

--- src/MyCodeCamp/Controllers/SpeakersController.cs
+++ src/MyCodeCamp/Controllers/SpeakersController.cs
@@ -2,8 +2,10 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using MyCodeCamp.Data;
+using MyCodeCamp.Data.Entities;
 using MyCodeCamp.Models;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace MyCodeCamp.Controllers
 {
@@ -49,6 +51,31 @@ namespace MyCodeCamp.Controllers
                 if (speaker.Camp.Moniker != moniker) return BadRequest("Speaker not specified camp");
 
                 return Ok(_mapper.Map<SpeakerModel>(speaker));
+            }
+            catch
+            {
+            }
+
+            return BadRequest();
+        }
+
+        public async Task<ActionResult> Post(string moniker, [FromBody]SpeakerModel model)
+        {
+            try
+            {
+                var camp = _repo.GetCampByMoniker(moniker);
+                if (camp == null) return NotFound($"Camp {moniker} was not found");
+
+                var speaker = _mapper.Map<Speaker>(model);
+                speaker.Camp = camp;
+
+                _repo.Add(speaker);
+
+                if (await _repo.SaveAllAsync())
+                {
+                    var url = Url.Link("SpeakerGet", new { moniker = camp.Moniker, id = speaker.Id });
+                    return Created(url, _mapper.Map<SpeakerModel>(speaker));
+                }
             }
             catch
             {
