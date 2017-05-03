c7ca4ea7ffd301089700497f17bbc0e979735ea0
h1416 <hieuvo1416@gmail.com>
2017-05-03 13:07:05 -0500

    Add speakers controller, a association controller

---
+++ src/MyCodeCamp/Controllers/SpeakersController.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MyCodeCamp.Data;
+
+namespace MyCodeCamp.Controllers
+{
+    [Route("api/camps/{moniker}/speakers")]
+    public class SpeakersController : BaseController
+    {
+        private ILogger<CampsController> _logger;
+        private IMapper _mapper;
+        private ICampRepository _repo;
+
+        public SpeakersController(ICampRepository repo,
+            ILogger<CampsController> logger,
+            IMapper mapper)
+        {
+            _repo = repo;
+            _logger = logger;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public void Get(string moniker)
+        {
+
+        }
+    }
+}
