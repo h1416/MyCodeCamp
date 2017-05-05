2f85dd066bd886451cc772c7503bd178ddd548b4
h1416 <hieuvo1416@gmail.com>
2017-05-05 14:25:24 -0500

    Adding authentication controller

---
+++ src/MyCodeCamp/Controllers/AuthController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using MyCodeCamp.Data;
+using MyCodeCamp.Data.Entities;
+
+namespace MyCodeCamp.Controllers
+{
+    public class AuthController : Controller
+    {
+        private CampContext _context;
+        private SignInManager<CampUser> _signInMgr;
+
+        public AuthController(CampContext context, SignInManager<CampUser> signInMgr)
+        {
+            _context = context;
+            _signInMgr = signInMgr;
+        }
+    }
+}
